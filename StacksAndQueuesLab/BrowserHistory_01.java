0fac37be585b0408365af7fd033c1ef58a1a1ea8
ZornitsaStoycheva <113451269+ZornitsaStoycheva@users.noreply.github.com>
2025-02-16 03:07:05 -0800

    add new directory

---
+++ StacksAndQueuesLab/BrowserHistory_01.java
@@ -0,0 +1,11 @@
+package StacksAndQueuesLab;
+
+import java.util.Scanner;
+
+public class BrowserHistory_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
---
+++ StacksAndQueuesLab/SimpleCalculator_02.java
@@ -0,0 +1,4 @@
+package StacksAndQueuesLab;
+
+public class SimpleCalculator_02 {
+}
