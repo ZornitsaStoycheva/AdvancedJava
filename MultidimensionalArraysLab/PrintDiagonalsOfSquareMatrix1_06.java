d32f71535567d89e0a055e8e630d6fd8c8cfae4f
ZornitsaStoycheva <113451269+ZornitsaStoycheva@users.noreply.github.com>
2025-02-19 11:50:40 -0800

    Print Diagonals of Square Matrix 1 ​

--- MultidimensionalArraysLab/PrintDiagonalsOfSquareMatrix1_06.java
+++ MultidimensionalArraysLab/PrintDiagonalsOfSquareMatrix1_06.java
@@ -1,11 +1,33 @@
 package MultidimensionalArraysLab;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PrintDiagonalsOfSquareMatrix1_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = Arrays.stream(scanner.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (row == col) {
+                    System.out.print(matrix[row][col] + " ");
+                }
+            }
+        }
+        System.out.println();
+
+        for (int row = matrix.length - 1, col = 0; col < matrix.length && row >= 0; row--, col++) {
+            System.out.print(matrix[row][col] + " ");
+        }
     }
 }
