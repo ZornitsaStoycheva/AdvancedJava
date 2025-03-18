b0b851d96e2e2ecab2f293ad585561df5913c1bc
ZornitsaStoycheva <113451269+ZornitsaStoycheva@users.noreply.github.com>
2025-03-18 05:59:54 -0700

    sum lines

--- StreamsFilesAndDirectoriesExercises/SumLines_01.java
+++ StreamsFilesAndDirectoriesExercises/SumLines_01.java
@@ -1,7 +1,28 @@
 package StreamsFilesAndDirectoriesExercises;
 
+import java.io.*;
+
 public class SumLines_01 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("resources/input.txt"));
+
+        String line = bufferedReader.readLine();
+
+        while (line != null) {
+
+            long sum = 0;
+
+            char[] characterFromLine = line.toCharArray();
+
+            for(char character : characterFromLine) {
+                sum += character;
+            }
+
+            System.out.println(sum);
+            line = bufferedReader.readLine();
+        }
+
+        bufferedReader.close();
     }
 }
