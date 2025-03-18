509d0780374336d2c060b7ce603634bdaa479078
ZornitsaStoycheva <113451269+ZornitsaStoycheva@users.noreply.github.com>
2025-03-18 07:18:28 -0700

    all capitals

---
+++ StreamsFilesAndDirectoriesExercises/AllCapitals_03.java
@@ -0,0 +1,27 @@
+package StreamsFilesAndDirectoriesExercises;
+
+import java.io.*;
+
+public class AllCapitals_03 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("resources/input.txt"));
+
+
+        PrintWriter printer = new PrintWriter(new FileWriter("all-capitals.txt"));
+
+        String line = bufferedReader.readLine();
+
+        while (line != null) {
+            String output = line.toUpperCase();
+            printer.print(output);
+
+            printer.println();
+
+            line = bufferedReader.readLine();
+        }
+
+        bufferedReader.close();
+        printer.close();
+    }
+}
---
+++ StreamsFilesAndDirectoriesExercises/SumBytes_02.java
@@ -0,0 +1,31 @@
+package StreamsFilesAndDirectoriesExercises;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SumBytes_02 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("resources/input.txt"));
+
+        String line = bufferedReader.readLine();
+
+        long sum = 0;
+
+        while (line != null) {
+
+            char[] characterFromLine = line.toCharArray();
+
+            for(char character : characterFromLine) {
+                sum += character;
+            }
+
+            line = bufferedReader.readLine();
+        }
+
+        System.out.println(sum);
+        bufferedReader.close();
+    }
+}
