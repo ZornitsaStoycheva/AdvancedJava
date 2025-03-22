0cca28693bbacbf4e1613dbd5db231012d082909
ZornitsaStoycheva <113451269+ZornitsaStoycheva@users.noreply.github.com>
2025-03-22 01:13:28 -0700

    line numbers

---
+++ StreamsFilesAndDirectoriesExercises/LineNumbers_05.java
@@ -0,0 +1,7 @@
+package StreamsFilesAndDirectoriesExercises;
+
+public class LineNumbers_05 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ StreamsFilesAndDirectoriesExercises/WordCount_06.java
@@ -0,0 +1,7 @@
+package StreamsFilesAndDirectoriesExercises;
+
+public class WordCount_06 {
+    public static void main(String[] args) {
+        
+    }
+}
